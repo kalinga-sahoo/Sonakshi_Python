75b31525b30b5d37112ac6e39aed3e8911b4f706
kalinga.sahoo@gmail.com <kalinga.sahoo@gmail.com>
2026-02-21 14:57:18 +0530

    initial commit

---
+++ BMI.py
@@ -0,0 +1,6 @@
+#calculating simple interest
+P=int(input("Enter principle:"))
+R=int(input("Enter rate:"))
+T=int(input("Enter time:"))
+simple_interest=(P*R*T)/100
+print("The simple interest is:",simple_interest)
---
+++ Name.py
@@ -0,0 +1,7 @@
+#To varify the name
+Name=str(input("Enter name:"))
+my_name="Sonakshi"
+if(Name==my_name):
+    print("This is the correct name")
+else:
+    print("The is not correct")
